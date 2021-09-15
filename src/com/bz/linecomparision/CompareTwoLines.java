0168c2ed923ac3a04ac446ba65db68530128a20a
Subhamdas20 <subham21.sd@gmail.com>
2021-09-15 14:30:20 +0530

    compare length of line using compareTO method and using functions

--- src/com/bz/linecomparision/CompareTwoLines.java
+++ src/com/bz/linecomparision/CompareTwoLines.java
@@ -1,6 +1,19 @@
 package com.bz.linecomparision;
 import java.util.*;
 public class CompareTwoLines {
+    static void compareLinesUsingMethods(int x1 , int x2 , int y1 , int y2 , int X1 , int X2 , int Y1 , int Y2){
+        Float lenth_line1 = (float) Math.sqrt((Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)));
+        Float lenth_line2 = (float) Math.sqrt((Math.pow((X2 - X1), 2) + Math.pow((Y2 - Y1), 2)));
+        System.out.println("Length of line is 1" + " " + lenth_line1);
+        System.out.println("Length of line is 2" + " " + lenth_line2);
+        int compare = lenth_line1.compareTo(lenth_line2);
+        if(compare==0)
+            System.out.println("Length of Line 1 is equal to line 2");
+        else if (lenth_line1>lenth_line2)
+            System.out.println("Length of Line 1 is greater");
+        else
+            System.out.println("Length of Line 2 is greater");
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of co-ordinate of  line 1 x1");
@@ -19,16 +32,7 @@ public class CompareTwoLines {
         int Y1 = sc.nextInt();
         System.out.println("Enter the value of co-ordinate of Y1 of line 2");
         int Y2 = sc.nextInt();
-        Float lenth_line1 = (float) Math.sqrt((Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)));
-        Float lenth_line2 = (float) Math.sqrt((Math.pow((X2 - X1), 2) + Math.pow((Y2 - Y1), 2)));
-        System.out.println("Length of line is 1" + " " + lenth_line1);
-        System.out.println("Length of line is 2" + " " + lenth_line2);
-        int compare = lenth_line1.compareTo(lenth_line2);
-        if(compare==0)
-            System.out.println("Length of Line 1 is equal to line 2");
-        else if (lenth_line1>lenth_line2)
-            System.out.println("Length of Line 1 is greater");
-        else
-            System.out.println("Length of Line 2 is greater");
+        compareLinesUsingMethods(x1,x2,y1,y2,X1,X2,Y1,Y2);
+
     }
 }
