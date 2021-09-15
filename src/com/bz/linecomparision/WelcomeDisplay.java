85eed7a8401900f7c114543fc1c6c48335631bbe
Subhamdas20 <subham21.sd@gmail.com>
2021-09-15 14:14:52 +0530

    Calculate length of line using geometry

--- src/com/bz/linecomparision/WelcomeDisplay.java
+++
@@ -1,7 +0,0 @@
-package com.bz.linecomparision;
-
-public class WelcomeDisplay {
-    public static void main(String[] args) {
-        System.out.println("Welcome to line comparison computation program");
-    }
-}
